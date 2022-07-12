c61e41731c101da94f8fe1ee3054725a6b123b5a
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-07-12 18:05:13 +0530

    Update backend.tf

--- project/backend.tf
+++ project/backend.tf
@@ -1,7 +1,8 @@
 # Configure the Google Cloud tfstate file location
 terraform {
   backend "gcs" {
-    bucket = "terraform-admin-demo"
+    // bucket = "terraform-admin-demo"
+    bucket = "db-ws9kiam-admin-demo"
     prefix = "terraform-project"
   }
 }
