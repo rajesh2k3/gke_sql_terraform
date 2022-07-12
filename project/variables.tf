f83437e7068d2f3f7f54690880f5ff6df3b170f1
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-07-12 18:09:52 +0530

    Update variables.tf

--- project/variables.tf
+++ project/variables.tf
@@ -1,15 +1,22 @@
 # GCP variables
 variable "region" {
-  default     = "asia-southeast1"
+  // default     = "asia-southeast1"
+  default     = "us-central1"
   description = "Region of resources"
 }
+
+//variable "zone" {
+//  description = "Zone of resources"
+//  default     = "us-central1-b"
+//}
+
 
 variable "project_name" {
   #  default     = "test"
 
   default = {
-    prod = "prod"
     dev  = "dev"
+    prod = "prod"
   }
 
   description = "The NAME of the Google Cloud project"
